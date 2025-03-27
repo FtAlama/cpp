c481dc762604b993c7db685f5244465ec330e508
Ajit Lama <alama@shi-r3-p2.s19.be>
2025-03-27 17:00:01 +0100

    test

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -5,24 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alama <alama@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/26 17:12:16 by alama             #+#    #+#             */
-/*   Updated: 2025/03/27 15:54:01 by alama            ###   ########.fr       */
+/*   Created: 2025/03/27 16:40:27 by alama             #+#    #+#             */
+/*   Updated: 2025/03/27 16:59:09 by alama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <string>
+#include <iostream>
 
 int	main(void)
 {
-	Zombie*	z;
-	int	a;
+	std::string str("HI THIS IS BRAIN");
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
-	a = 90;
-	z = zombieHorde(a, "zzz");
-
-	for (int i = 0; i < a; i++)
-	{
-		z[i].announce();
-	}
-	delete [] z;
+	std::cout << "The memory adress of the string:	"
+	<< &str << std::endl
+	<< "The memory address held by stringPTR:	"
+	<< &stringPTR << std::endl
+	<< "the memory address held by stringREF:	"
+	<< &stringREF << std::endl
+	<< "The value of the string variable: 	"
+	<< str << std::endl
+	<< "The value pointed by stringPTR: 	"
+	<< *stringPTR << std::endl 
+	<< "The value pointed by stringREF: 	"
+	<< stringREF << std::endl; 
 }
